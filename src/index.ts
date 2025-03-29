dbbf7ebeb2bf7f5080a26837d7636e17ff190dbb
ElianArism <elianarism@gmail.com>
2025-03-29 17:04:05 -0300

    feat: finish first demo of chat global start learning how socket io works

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,33 @@ import express from "express";
 import { createServer } from "node:http";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
 
 const app = express();
 
 const sv = createServer(app);
 
+const io = new Server(sv);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "..", "/public", "/index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A new user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("to-sv-message", (msg) => {
+    console.log("Message received: " + msg);
+    io.emit("to-client-message", msg);
+  });
 });
 
 sv.listen(3000, () => {
